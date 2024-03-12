16e53ecfd639a240b63cf875e868b768b7e65bdd
Shivam chaudhary <137470229+shivamgithubok@users.noreply.github.com>
2024-03-12 23:20:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// let drum= document.querySelectorAll(".drum").length;
+// for(let i=0; i< drum; i++){
+// document.querySelectorAll(".drum")[i].addEventlistener("click",function(){
+//    alert("ok");
+// })
+// }
